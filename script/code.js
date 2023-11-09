faed35c2fc15536a6e799521e2b1abdcb88efb3f
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-11-09 09:16:40 +0200

    formDone

--- script/code.js
+++ script/code.js
@@ -1,5 +1,12 @@
-function addition(){
-    console.log(numb1 + numb2)
+let numb1 =document.getElementById('numb1')
+let numb2 =document.getElementById('numb2')
+
+function calculateResult(){
+    let total = +numb1.value + +numb2.value
+    let sum = document.getElementById('output')
+    sum.innerHTML = total
 }
-const button = document.getElementById(btn)
-btnAddition
+
+const btnSubmit = document.getElementById('btn')
+btnSubmit.addEventListener('click', calculateResult)
+
