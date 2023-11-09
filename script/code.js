a9313b4a2b62fb1f36edf62a30073d185f7bdc7f
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-11-09 09:50:17 +0200

    rubricAdd

--- script/code.js
+++ script/code.js
@@ -10,3 +10,15 @@ function calculateResult(){
 const btnSubmit = document.getElementById('btn')
 btnSubmit.addEventListener('click', calculateResult)
 
+/*const btnAdd =document.queryselector('[data-submit]')
+
+const lblOutput =document.querySelector ('#output')
+
+function addition() {
+    let numb1 = document.getElementById('numb1').value
+    let numb1 = document.getElementById('numb2').value
+    let sum = +numb1 + +numb2
+    lblOutput.textContent = sum
+}
+btnAdd.addEventListener('click', addition)
+eval(`${numb1}+ ${numb2}`)*/
